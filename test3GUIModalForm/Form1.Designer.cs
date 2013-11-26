d284ec43e061c42dbefce2ea69fdd3070bcdb0a5
eveil <erick@erickveil.net>
2013-11-26 09:42:18 -0800

    First commit

---
+++ test3GUIModalForm/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace test3GUIModalForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mainBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Launch Form";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(108, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Form Results";
+            // 
+            // mainBox
+            // 
+            this.mainBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::test3GUIModalForm.Properties.Settings.Default, "msg", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.mainBox.Location = new System.Drawing.Point(12, 54);
+            this.mainBox.Name = "mainBox";
+            this.mainBox.ReadOnly = true;
+            this.mainBox.Size = new System.Drawing.Size(268, 20);
+            this.mainBox.TabIndex = 2;
+            this.mainBox.Text = global::test3GUIModalForm.Properties.Settings.Default.msg;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 88);
+            this.Controls.Add(this.mainBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox mainBox;
+    }
+}
+
---
+++ test3GUIModalForm/Form2.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace test3GUIModalForm
+{
+    partial class modalForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sendBox = new System.Windows.Forms.TextBox();
+            this.sendText = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // sendBox
+            // 
+            this.sendBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::test3GUIModalForm.Properties.Settings.Default, "msg", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.sendBox.Location = new System.Drawing.Point(12, 12);
+            this.sendBox.Name = "sendBox";
+            this.sendBox.Size = new System.Drawing.Size(268, 20);
+            this.sendBox.TabIndex = 0;
+            this.sendBox.Text = global::test3GUIModalForm.Properties.Settings.Default.msg;
+            // 
+            // sendText
+            // 
+            this.sendText.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.sendText.Location = new System.Drawing.Point(105, 38);
+            this.sendText.Name = "sendText";
+            this.sendText.Size = new System.Drawing.Size(75, 23);
+            this.sendText.TabIndex = 1;
+            this.sendText.Text = "sendText";
+            this.sendText.UseVisualStyleBackColor = true;
+            this.sendText.Click += new System.EventHandler(this.sendText_Click);
+            // 
+            // modalForm
+            // 
+            this.AcceptButton = this.sendText;
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.Dialog;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 76);
+            this.Controls.Add(this.sendText);
+            this.Controls.Add(this.sendBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "modalForm";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox sendBox;
+        private System.Windows.Forms.Button sendText;
+    }
+}
